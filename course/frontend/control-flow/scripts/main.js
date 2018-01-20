04269418176e3e4968865c006d2c3b8d308f0a08
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-01-20 17:12:42 +0100

    correction of the exercise under100

--- course/frontend/control-flow/scripts/main.js
+++ course/frontend/control-flow/scripts/main.js
@@ -133,7 +133,7 @@ console.log('7) '); contructPattern();
 
 function under100(){
     var num = 0;
-    for(i=0;i>1000;i++){
+    for(i=0;i<1000;i++){
         if(i%3==0 && i%5==0){
             num = num + i;
         }
